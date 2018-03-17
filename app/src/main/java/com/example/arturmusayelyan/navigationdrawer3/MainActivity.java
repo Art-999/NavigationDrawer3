0947d0f036ab70688e54dbe80661c0739a6b28ca
artur.musayelyan <musayelyan099@gmail.com>
2018-03-17 18:07:36 +0400

    full example create

--- app/src/main/java/com/example/arturmusayelyan/navigationdrawer3/MainActivity.java
+++ app/src/main/java/com/example/arturmusayelyan/navigationdrawer3/MainActivity.java
@@ -2,13 +2,16 @@ package com.example.arturmusayelyan.navigationdrawer3;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DrawerLayout.DrawerListener {
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
@@ -20,17 +23,55 @@ public class MainActivity extends AppCompatActivity {
 
         // toolbar = findViewById(R.id.app_bar);
         //setSupportActionBar(toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();//mer drac toolbar-n e mez et veradarznum
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
         drawerLayout = findViewById(R.id.drawer_layout);
-        navigationView=findViewById(R.id.navigation_view);
+        drawerLayout.addDrawerListener(this);//vor drawer listener-e set lini vran
+        navigationView = findViewById(R.id.navigation_view);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
 
                 menuItem.setChecked(true);
-                //drawerLayout.closeDrawers();
+                drawerLayout.closeDrawers();
 
                 return false;
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onDrawerSlide(View drawerView, float slideOffset) {
+        // Respond when the drawer's position changes
+
+    }
+
+    @Override
+    public void onDrawerOpened(View drawerView) {
+// Respond when the drawer is opened
+    }
+
+    @Override
+    public void onDrawerClosed(View drawerView) {
+// Respond when the drawer is closed
+    }
+
+    @Override
+    public void onDrawerStateChanged(int newState) {
+// Respond when the drawer motion state changes
     }
 }
