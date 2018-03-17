492e43ddf291f625749d0790a841e09480cec175
artur.musayelyan <musayelyan099@gmail.com>
2018-03-17 17:07:50 +0400

    Initial commit

---
+++ app/src/main/java/com/example/arturmusayelyan/navigationdrawer3/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.arturmusayelyan.navigationdrawer3;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // toolbar = findViewById(R.id.app_bar);
+        //setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView=findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                menuItem.setChecked(true);
+                //drawerLayout.closeDrawers();
+
+                return false;
+            }
+        });
+    }
+}
